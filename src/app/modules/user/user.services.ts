3be485b51c41c1bdb8fcc7c5d32b00b2a4fb9ff6
riyazulhaque64 <riyazulhaque64@gmail.com>
2025-12-19 02:21:58 +0600

    update

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,8 +1,15 @@
+import bcrypt from "bcrypt";
 import { db } from "../../../db";
 import { users } from "../../../db/schema";
+import config from "../../config";
 
 // ----------------------------------- CREATE USER ------------------------------------
 const createUser = async (data: any) => {
+  const hashedPassword = await bcrypt.hash(
+    data.password,
+    config.salt_rounds
+  );
+
   const result = await db.insert(users).values(data).returning();
   return result;
 };
