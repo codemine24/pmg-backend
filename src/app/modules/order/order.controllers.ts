801aeacc2151f6d0234fc9a3b9cb4b3373513c7e
riyazulhaque64 <riyazulhaque64@gmail.com>
2026-01-03 20:35:39 +0600

    update

--- src/app/modules/order/order.controllers.ts
+++ src/app/modules/order/order.controllers.ts
@@ -208,27 +208,6 @@ const progressOrderStatus = catchAsync(async (req, res) => {
     });
 });
 
-// ----------------------------------- GET CLIENT DASHBOARD SUMMARY ----------------------------
-const getClientDashboardSummary = catchAsync(async (req, res) => {
-    const user = (req as any).user;
-    const platformId = (req as any).platformId;
-
-    // Get company ID from user
-    const companyId = user.company_id;
-    if (!companyId) {
-        throw new CustomizedError(httpStatus.BAD_REQUEST, "Company ID is required");
-    }
-
-    const result = await OrderServices.getClientDashboardSummary(companyId, platformId);
-
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Dashboard summary fetched successfully",
-        data: result,
-    });
-});
-
 // ----------------------------------- GET ORDER STATUS HISTORY ---------------------------
 const getOrderStatusHistory = catchAsync(async (req, res) => {
     const user = (req as any).user;
@@ -432,7 +411,6 @@ export const OrderControllers = {
     updateJobNumber,
     getOrderScanEvents,
     progressOrderStatus,
-    getClientDashboardSummary,
     getOrderStatusHistory,
     updateTimeWindows,
     getPricingReviewOrders,
